224e520b16c2d65a04550b9f62b942649c994596
Gohar Abbas <103553225+khadimhussain110@users.noreply.github.com>
2023-02-04 06:38:39 -0800

    Add files via upload
    5th js assgiment

---
+++ app.js
@@ -0,0 +1,153 @@
+// Chapter 5 Assignment # 5 JAVASCRIPT
+
+// question 1
+
+// var a =5;
+// var b =3;
+// var z =(a + b);
+// document.write("Sum of 3 and 5 " + z);
+
+// question 2
+
+//(---Addition---)
+
+// var Num1 = 2 + 2;
+// alert(Num1);
+
+// (--Subtraction--)
+
+// var Num1 = 14 - 5;
+// alert(Num1);
+
+// (--Multiplication--)
+
+// var Num1 = 20 * 9;
+// alert(Num1);
+
+// (--	Modulus Division Remainder--)
+
+// var Num1 = 25 % 14;
+//    alert(Num1);
+
+// (--division--)
+
+// var Num1 = 30 / 18;
+//    alert(Num1);
+
+// question 3
+
+// var a =
+
+// document.write("Value after variable declaration is: " + a);
+
+// question 4 (a)
+
+// question 4 (c)
+// var a =("");
+
+// var b =("5")
+// document.write("Initial value: " + a + b);
+
+// question 4 (e)
+// var a =5;
+// document.write("Value after Increment:" + ++a);
+
+// question 4 (g)
+
+// var a =7 + 6;
+// var b =(a);
+// document.write("Value after addition is: " + a);
+
+// question 4 (i)
+
+// var a =(13);
+// document.write("Value after Decrement is: " + --a );
+
+// question 4 (k)
+
+// var a =10 % 2;
+// document.write("The remainder is " + a);
+
+// qustion 4
+
+// var cost =(600 * 5);
+// document.write("Total cost to buy 5 tickets to a movie is " + cost);
+
+// question 5
+
+// var i = 1, table = prompt("enter table no"), result;
+
+// do {
+//     result = table * i;
+//     document.write(" x " + i + " = " + result + "<br>");
+//     i++
+// }
+// while (i <= 10)
+
+// question 6
+
+// is pending concept is not clear
+
+// question 7
+
+// function cToF(celsius) 
+// {
+//   var cTemp = celsius;
+//   var cToFahr = cTemp * 9 / 5 + 32;
+//   var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+//     console.log(message);
+//     document.write(message);
+// }
+
+// function fToC(fahrenheit) 
+// {
+//   var fTemp = fahrenheit;
+//   var fToCel = (fTemp - 32) * 5 / 9;
+//   var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+//     console.log(message);
+//     document.write(message);
+// } 
+// cToF(60);
+// fToC(45);
+
+// question 7
+
+var item1 = 650;
+var item2 = 100;
+var qty1 = 3;
+var qty2 = 7;
+var shappingcharges = 100;
+var totalcost_of1item = (item1 * qty1);
+var totalcost_of2item = (item2 * qty2);
+// document.write(totalcost_of1item + totalcost_of2item + shappingcharges);
+// console.log(totalcost_of1item + totalcost_of2item + shappingcharges);
+
+// question 8
+
+// var a =980;
+// var b =804;
+// var c = b / a * 100;
+// document.write("perecntage " + c);
+
+//  question 9
+
+// var us_doller =277.81;
+// var saudi_riyal=74.04;
+// var qty1=10;
+// var qty2=25;
+// var total1=us_doller * qty1;
+// var total2=saudi_riyal * qty2;
+// document.write(total1 + total2);
+
+// question 10
+
+// var a= 2 + 3;
+// document.write("addtion " + a);
+// var b= 10 - 5;
+// document.write("multiply " + b );
+// var c=a / b ;
+// document.write(c);
+
+
+
+
